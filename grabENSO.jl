fc8f6860e7ed43d7b890df7083a35e6e1b5a1ad4
Levi Silvers <C823281551@luft.atmos.colostate.edu>
2024-08-08 16:57:31 -0600

    create arrays for positive and negative phases of ENSO

--- grabENSO.jl
+++ grabENSO.jl
@@ -23,7 +23,7 @@ equals_yr(year::String7) = year == " 2005"
 
 whichy = [1965, 1970, 1975, 1980, 1985]
 
-istart = 63
+istart = 2
 iend   = 73
 for i in istart:73
     if i < istart + 1 
@@ -40,13 +40,38 @@ end
 # broadcast (using the '.') the parse function to apply to vector.
 enso34 = parse.(Float64,c)
 
+print(enso34)
+print("*************************")
+len = size(enso34)
+nino = zeros(len[1])
+nina = zeros(len[1])
+
+# define thresholds for the positive and negative phases of ENSO:
+up = 1
+dn = -1
+
+for i in istart:len[1]
+    if enso34[i] > up 
+        nino[i] = 1.
+    elseif enso34[i] < dn
+        nina[i] =-1.
+    else
+        nino[i] = 0.
+        nina[i] = 0.
+    end
+end
+
+print(nina)
+
 fig = Figure()
 ax = Axis(fig[1,1];
     xlabel="monthly mean values",
     ylabel="anomaly",
-    title="ENSO index over 12 years"
+    title="ENSO index"
     )
-lines!(ax, enso34[:], linewidth = 3.0)
+lines!(ax, enso34[:], linewidth = 1.5)
+lines!(ax, nino[:], linewidth = 3.0)
+lines!(ax, nina[:], linewidth = 3.0)
 #
 save("plotENSOeasy.png",fig)
 
