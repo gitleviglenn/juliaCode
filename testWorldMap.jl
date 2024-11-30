6ad9b44974714980fd89dd8b1af54fdcb9307c4b
Levi Silvers <C823281551@luft.lan>
2024-11-30 11:43:54 -0700

    first commit for testWorldMap.jl script for plotting rh or sst values

---
+++ testWorldMap.jl
@@ -0,0 +1,82 @@
+using CairoMakie
+using GeoMakie
+using NCDatasets
+
+path="/Users/C823281551/data/"
+modelp="MPI-ESM1-2-LR" 
+#filehur  = path*"cmip6/MPIESM/hur_Amon_"*modelp*"_ssp585_r1i1p1f1_gn_20150116-21001216.nc" 
+
+filein  = path*"cmip6/MPIESM/hur_Amon_"*modelp*"_ssp585_r1i1p1f1_gn_20150116-21001216_regridded3.nc" 
+#filein  = path*"cmip6/MPIESM/tos_Omon_"*modelp*"_ssp585_r1i1p1f1_gn_20150116-21001216_pm40b.nc" 
+
+data  = NCDataset(filein)
+
+data.attrib
+
+#var   = data["hur"] # hur(time, plev, lat, lon)
+#var  = data["tos"] # hur(time, plev, lat, lon)
+lat  = data["lat"]
+lon  = data["lon"]
+tme = data["time"]
+#lev  = data["plev"]
+
+## tos
+#var  = data["tos"] # hur(time, plev, lat, lon)
+#tit = "tos"
+#lat1=1
+#lat2=40
+
+# rh
+var   = data["hur"] # hur(time, plev, lat, lon)
+tit = "rh"
+lat1 = 51
+lat2 = 130
+
+tim = 5 # we need to grab rh values at the times selected by check_thresh
+#data_2_plot = var[:,lat1:lat2,tim]
+data_2_plot = var[:,lat1:lat2,2,tim]
+
+#rh1 = rh[:,:,1,tim]
+#rh2 = rh[:,:,2,tim]
+# rh
+#data_2_plot = var[:,51:130,2,tim]
+# tos
+#data_2_plot = var[:,:,tim]
+#s = circshift(lon, 180) # passing this in as the lon dimension shifts the rh but not coastlines. 
+
+function fig_1_plot(inpv,d1,d2,tit)
+    f2 = Figure(;
+        figure_padding=(5,5,10,10),
+        backgroundcolor=:snow2,
+        size=(600,300),
+        )
+    ax = Axis(f2[1,1];
+        xticks = -180:30:180, 
+        #xticks = 0:30:360, 
+        yticks = -90:30:90,
+        ylabel="latitude",
+        xlabel="longitude",
+        limits=(-180,180,-40,40),
+        title=tit,
+        )
+        bb = contourf!(ax, d1, d2, inpv, 
+             #levels = range(0, 40, length = 10), # tos
+             levels = range(0, 100, length = 10), # rh
+             colormap = :Blues_8,
+             #colormap = :navia,
+             #colormap = :batlow,
+             #colormap = :vik,
+        )
+        lines!(ax, GeoMakie.coastlines())
+        Colorbar(f2[1,2], bb)
+    return f2
+end
+
+# rh
+#data_2_plot = var[:,51:130,2,tim]
+# tos
+#data_2_plot = var[:,:,tim]
+#s = circshift(lon, 180) # passing this in as the lon dimension shifts the rh but not coastlines. 
+
+#fig = fig_1_plot(data_2_plot,lon,lat[51:130],"relative humidity")
+fig = fig_1_plot(data_2_plot,lon,lat[lat1:lat2],tit)
