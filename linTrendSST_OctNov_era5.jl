4c9c334d3668456463c6ce0ae3ec967d00f46158
Levi Silvers <silversster@gmail.com>
2025-08-07 16:51:58 -0600

    begin changes to generalize linTrendSST_OctNov_era5.jl for other variables.

--- linTrendSST_OctNov_era5.jl
+++ linTrendSST_OctNov_era5.jl
@@ -13,8 +13,10 @@ include("ensoFuncs.jl")
 
 path="/Users/C823281551/data/ERA5/"
 
-filein  = path*"era5_sst_1979th2024_OctNov_360x180.nc"
+filein1 = path*"era5_sst_1979th2024_OctNov_360x180.nc"
+filein  = path*"MPI_ERA5_full_output.nc"
 tag = "ERA5"
+data1  = NCDataset(filein1)
 data   = NCDataset(filein)
 
 #lat = data["latitude"]
@@ -23,12 +25,32 @@ lat = data["lat"]
 lon = data["lon"]
 tme = data["valid_time"]
  
-sst_var = data["sst"]
+dim_var = data1["sst"]
 
 timeAxis1 = collect(1:1:92);
 
-sst_var = data["sst"];
-dims    = size(sst_var)
+#sst_var = data["sst"];
+sst_var = data["vmax"];
+dims    = size(dim_var)
+
+# vmax has 408 timesteps.... sst_var has 92
+# 10,11,22,23
+#
+# save i and i+1, jump 12, do again
+for i in 10:12:408
+  println("hah!")
+  #j = i 
+  #println(j)
+  #j = j+1
+  #println(j)
+  #j = j+12
+  println(i)
+  i = i+1
+  println(i)
+end 
+
+print("size of sst Array is: ",size(dim_var))
+print("size of PI Array is: ",size(sst_var))
 
 agrid  = Array{Union{Missing, Float64}, 2}(undef, dims[1], dims[2])
 bgrid  = Array{Union{Missing, Float64}, 2}(undef, dims[1], dims[2])
@@ -36,10 +58,11 @@ bgrid  = Array{Union{Missing, Float64}, 2}(undef, dims[1], dims[2])
 for i in 1:dims[1]
     for j in 1:dims[2]
         agrid[i,j],bgrid[i,j] = find_best_fit(timeAxis1[:],sst_var[i,j,:])
+        #agrid[i,j],bgrid[i,j] = find_best_fit(timeAxis1[:],sst_var[:,i,j])
     end
 end
 
 levs = range(-.5, .5, length = 21)
 blah = fig_anom_plot(agrid.*20,lon,lat,"linear trends October-November (C/decade)",levs)
 
-save("era5_sst_LinTrend_OctThNov_1979th2024.png", blah, px_per_unit=6.0)
+save("era5_PI_LinTrend_OctThNov_1979th2024.png", blah, px_per_unit=6.0)
